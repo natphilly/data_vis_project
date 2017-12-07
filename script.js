4d73d190b2b202d85e7efd80379ab0cc165ef6fb
natphilly <phillnab@uni.edu>
2017-12-06 22:29:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var piemenu = new wheelnav('piemenu');
+piemenu.spreaderInTitle = icon.plus;
+piemenu.spreaderOutTitle = icon.cross;
+piemenu.spreaderRadius = piemenu.wheelRadius * 0.34;
+piemenu.wheelRadius = piemenu.wheelRadius * 0.83;
+piemenu.createWheel();
